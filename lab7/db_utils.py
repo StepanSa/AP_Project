5775090a860468b0866dc71ee297bda8e7c3d1d0
Stepan Sarabun <stepankolv12@gmail.com>
2022-11-14 23:24:44 +0200

    lab 7 done

--- lab7/db_utils.py
+++ lab7/db_utils.py
@@ -1,4 +1,12 @@
-from lab6.models import Session
+from sqlalchemy import desc, exists
+
+from lab6.models import Session, User, Ticket
+from lab7.schemas import PlaceOrder
+
+
+def is_id_taken(model_class, id):
+    session = Session()
+    return session.query(exists().where(model_class.id == id)).scalar()
 
 
 def create_entry(model_class, *, commit=True, **kwargs):
@@ -8,6 +16,81 @@ def create_entry(model_class, *, commit=True, **kwargs):
     if commit:
         session.commit()
     return entry
+
+
+def get_entry_by_id(model_class, id, **kwargs):
+    session = Session()
+    if session.query(model_class).filter_by(id=id, **kwargs).all() == []:
+        return 400
+    return session.query(model_class).filter_by(id=id, **kwargs).one()
+
+
+def get_entry_user(model_class, id, **kwargs):
+    session = Session()
+    if session.query(model_class).filter_by(id=id, **kwargs).all() == []:
+        return 400
+    return session.query(model_class).filter_by(userId=id, **kwargs).all()
+
+
+def get_entry_all(model_class):
+    session = Session()
+    return session.query(model_class).all()
+
+
+def get_entry_self(model_class, id=1, **kwargs):
+    session = Session()
+    return session.query(model_class).filter_by(id=id, **kwargs).one()
+
+
+def delete_entry(model_class, id, commit=True, **kwargs):
+    session = Session()
+    session.query(model_class).filter_by(id=id, **kwargs).delete()
+    if commit:
+        session.commit()
+
+
+def update_entry(entry, *, commit=True, **kwargs):
+    session = Session()
+    for key, value in kwargs.items():
+        setattr(entry, key, value)
+    if commit:
+        session.commit()
+    return entry
+
+
+def delete_entry_self(model_class, id, commit=True, **kwargs):
+    session = Session()
+    session.query(model_class).filter_by(id, **kwargs).delete()
+    if commit:
+        session.commit()
+
+
+def create_order(commit=True, **orderinfo):
+    session = Session()
+
+    userid = orderinfo.get('userId')
+    ticketid = orderinfo.get('ticketId')
+
+    user = get_entry_by_id(User, userid)
+    ticket = get_entry_by_id(Ticket, ticketid)
+
+    order = PlaceOrder(**orderinfo, user=user, ticket=ticket)
+
+    session.add(order)
+    if commit:
+        session.commit()
+    return order
+
+
+def is_ticket_taken(model_class, id):
+    session = Session()
+    return session.query(exists().where(model_class.status == "approved", model_class.ticketId == id)).scalar()
+
+
+def is_name_taken(model_class, name):
+    session = Session()
+    return session.query(exists().where(model_class.username == name)).scalar()
+
 
 
 
--- lab7/schemas.py
+++ lab7/schemas.py
@@ -1,18 +1,19 @@
 from marshmallow import Schema, validate, fields
 from flask_bcrypt import generate_password_hash
 from datetime import date
+import json
 
 
-class User(Schema):
+class UserData(Schema):
     id = fields.Integer()
     username = fields.String()
     firstName = fields.String()
     lastName = fields.String()
-    email = fields.String(validate=validate.Email())
-    password = fields.Function()
-    phone = fields.Function()
+    email = fields.String()
+    password = fields.String()
+    phone = fields.String()
     birthDate = fields.Date()
-    userStatus = fields.Integer(validate=validate.OneOf(["0", "1"]))
+    userStatus = fields.Integer()
 
 
 class CreateUser(Schema):
@@ -23,3 +24,56 @@ class CreateUser(Schema):
     password = fields.Function(deserialize=lambda obj: generate_password_hash(obj), load_only=True)
     phone = fields.Function(validate=validate.Regexp('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[\s0-9]{4,20}$'))
     birthDate = fields.Date(validate=lambda x: x < date.today())
+
+
+class UpdateUser(Schema):
+    firstName = fields.String()
+    lastName = fields.String()
+    email = fields.String(validate=validate.Email())
+    password = fields.Function(deserialize=lambda obj: generate_password_hash(obj), load_only=True)
+    phone = fields.Function(validate=validate.Regexp('^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[\s0-9]{4,20}$'))
+
+
+class GetUser(Schema):
+    id = fields.Integer()
+    username = fields.String()
+    firstName = fields.String()
+    lastName = fields.String()
+    email = fields.String()
+    phone = fields.String()
+    birthDate = fields.Date()
+    userStatus = fields.Integer()
+
+
+class CreateTicket(Schema):
+    name = fields.String()
+    status = fields.String(validate=validate.OneOf(["free", "booked", "sold"]))
+    price = fields.Integer()
+
+
+class GetTicket(Schema):
+    id = fields.Integer()
+    name = fields.String()
+    status = fields.String()
+    price = fields.Integer()
+
+
+class UpdateTicket(Schema):
+    name = fields.String()
+    status = fields.String()
+    price = fields.Integer()
+
+
+class GetOrder(Schema):
+    id = fields.Integer()
+    ticketId = fields.Integer()
+    userId = fields.Integer()
+    status = fields.String()
+
+
+class PlaceOrder(Schema):
+    ticketId_dic = fields.Nested(GetTicket(only=("id",)))
+    ticketId = fields.List(fields.Pluck(GetTicket, "id"))
+
+    userId = fields.Nested(UserData(only=("id",)))
+    status = fields.String(validate=validate.OneOf(["placed", "approved", "denied"]))
